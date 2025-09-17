9b98aac9f7c373f8c86339358e87df182a09a8c3
Kiam Khan Limon <103204454+mdlimon0175@users.noreply.github.com>
2025-09-17 09:42:18 +0600

    Update appConfig.js

--- config/appConfig.js
+++ config/appConfig.js
@@ -1,7 +1,7 @@
 const appConfig = Object.freeze({
     app_name: process.env.NEXT_PUBLIC_APP_NAME ?? "Dynamic No-Code Form Builder",
-    app_url: process.env.NEXT_PUBLIC_APP_URL ?? "http://localhost:3000",
-    api_url: process.env.NEXT_PUBLIC_API_URL ?? "http://localhost:3000/api",
+    app_url: process.env.NEXT_PUBLIC_APP_URL ?? "https://form-builder-1tys37xe7-kiam-khan-limons-projects.vercel.app",
+    api_url: process.env.NEXT_PUBLIC_API_URL ?? "https://form-builder-1tys37xe7-kiam-khan-limons-projects.vercel.app/api",
 });
 
 export default appConfig;
