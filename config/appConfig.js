a42778ab93ed226e1701c7148db52a8645efc5cd
Kiam Khan Limon <103204454+mdlimon0175@users.noreply.github.com>
2025-09-17 15:53:27 +0600

    Update appConfig.js

--- config/appConfig.js
+++ config/appConfig.js
@@ -1,7 +1,7 @@
 const appConfig = Object.freeze({
     app_name: process.env.NEXT_PUBLIC_APP_NAME ?? "Dynamic No-Code Form Builder",
-    app_url: process.env.NEXT_PUBLIC_APP_URL ?? "https://form-builder-mu-five.vercel.app/",
-    api_url: process.env.NEXT_PUBLIC_API_URL ?? "https://form-builder-mu-five.vercel.app/api",
+    app_url: process.env.NEXT_PUBLIC_APP_URL ?? "http://localhost:3000",
+    api_url: process.env.NEXT_PUBLIC_API_URL ?? "http://localhost:3000/api",
 });
 
 export default appConfig;
